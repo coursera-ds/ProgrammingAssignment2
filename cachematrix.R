8474f7827d28db75924705cfed650999b37cb9bc
coursera-ds <coursera-ds@users.noreply.github.com>
2014-05-09 20:57:21 -0700

    Update cachematrix.R
    initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below compute the inverse of a matrix 
+## and cache it so it can be accessed quickly the next
+## time it is needed without having to recompute the
+## inverse.
 
-## Write a short comment describing this function
+## Usage:
+##
+## source('cachematrix.R')
+## M = matrix(c(1,2,3,4),2,2)
+## newf <- makeCacheMatrix()
+## newf$set(M)
+## cacheSolve(newf)
+## 
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCacheMatrix():
+##  Provides functions for getting/setting the matrix,
+##  and computing/caching the inverse.
+makeCacheMatrix <- function(x = matrix()) 
+{
+    m   <- NULL
+    
+    set <- function(y) 
+    {
+        x <<- y
+        m <<- NULL
+    }
+    
+    get         <- function() x
+    set_inverse <- function(inverse) m <<- inverse
+    get_inverse <- function() m
+    
+    list(set = set, get = get, set_inverse = set_inverse, get_inverse = get_inverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve()
+##  Attempts to retrieve the matrix inverse from
+##  cache and return it, if the inverse is not 
+##  cached, this function makes a call to 
+##  compute/cache the inverse.
+## 
+cacheSolve <- function(x, ...) 
+{
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$get_inverse()
+    
+    if(!is.null(m)) 
+    {
+        message("getting cached data")
+        return(m)
+    }
+    
+    data <- x$get()
+    m    <- solve(data)
+    x$set_inverse(m)
+    m
 }
